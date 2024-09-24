2eb358197d78e4b4b6f84d4002b6d71a16ec710c
Steelgrou <steelgrou03@gmail.com>
2024-09-24 14:50:34 +0500

    add footer

--- src/components/layouts/footer.jsx
+++ src/components/layouts/footer.jsx
@@ -1,25 +1,28 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import './footer.css'; // Импортируем стили (по желанию)
-
+import footerLogo from "/logo.webp"
 
 const Footer = () => {
-    return (
-       <footer className='footer'>
-         <div className="container">
-           <h3>LOGO</h3>
-           <div className="footer__location">
-             <h4>location</h4>
-           </div>
-           <div className="footer__phone">
-            <h4>phone</h4>
-           </div>
-         </div>
+  return (
+    <footer className='footer'>
+      <div className="container">
+        <div className="footer-logo">
+          <img src={footerLogo} alt="footer-logo" />
+          <p className="footer__text">Супер <br /> Сантехник</p>
+        </div>
+        <div className="footer__location">
+          <h4>location</h4>
+        </div>
+        <div className="footer__phone">
+          <h4>phone</h4>
+        </div>
+      </div>
 
-         <div className="footer__corporation">
-         <p>1231231241414</p>
-         </div>
-       </footer>
-    );
+      <div className="footer__corporation">
+        <p>1231231241414</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
