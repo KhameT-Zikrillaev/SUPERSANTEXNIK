cb13659f3af5320767c20f936cb7d94e080faa7e
KhameT-Zikrillaev <zxamidulla1995@mail.ru>
2024-09-24 18:37:23 +0300

    completed

--- src/components/layouts/main.jsx
+++ src/components/layouts/main.jsx
@@ -1,5 +1,6 @@
 import React,{ useState } from 'react';
 import './main.css'; // Импортируем стили (по желанию)
+import Browsers from '../../pages/browsers/browsers';
 import Intro from '../../pages/intro/intro';
 import About from '../../pages/about/about';
 import Contact from '../../pages/contact/contact';
@@ -9,7 +10,8 @@ const Main = () => {
 
     return (
      <main className='main'>
-        <Intro/>
+        <Browsers/>
+         <Intro/>
          <About/>
          <Contact/>
      </main>
