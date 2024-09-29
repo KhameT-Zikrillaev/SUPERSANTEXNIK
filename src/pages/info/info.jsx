b5fbc6b12fefbf2cc421fe545901dfb8a148c732
KhameT-Zikrillaev <zxamidulla1995@mail.ru>
2024-09-29 17:18:15 +0300

    completed

--- src/pages/info/info.jsx
+++ src/pages/info/info.jsx
@@ -10,6 +10,22 @@ const Info = () => {
           <div className="container">
              <h3 className='Info__text'>С  Нами <p>всё просто</p></h3>
           </div>
+
+
+
+           <div className="Info__cards">
+            <div className="Info__cards-card">
+                 <div className="Info__cards-card-items">
+                  <div className="Info__cards-card-items-top">
+                    <h3>1</h3> <span></span>
+                  </div>
+                  <div className="Info__cards-card-items-bottom">
+                    <h4>Принимаем заявку</h4>
+                      <h5>Вы позвонили по номеру, оставили заявку на сайте или написали в Telegram</h5>
+                  </div>
+                 </div>
+            </div>
+           </div>
          </section>
         </>
     );
